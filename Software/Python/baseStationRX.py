bcaefbc43bb7deb3c4fd9e280467a5338a2150ff
devRCR <renzo.chan@outlook.com>
2023-08-31 08:45:59 -0500

    POST with authentication added

--- Software/Python/baseStationRX.py
+++ Software/Python/baseStationRX.py
@@ -1,9 +1,9 @@
 import serial, time, json, requests, rel, websocket
 
 # Configura el puerto serial
-baseStation = serial.Serial('COM17', 115200)  # Cambia 'COM1' al nombre de tu puerto serial y 9600 a la velocidad correcta
+baseStation = serial.Serial('COM6', 115200)  # Cambia 'COM1' al nombre de tu puerto serial
 
-url = 'https://appsqa.essalud.gob.pe/gestion-atencion-service/api/ticket/atender-cola'
+url = 'https://appsqa.essalud.gob.pe/sagw/gestatenpuls/atender-cola'
 headers = {'Content-type': 'application/json'}
 
 try:
@@ -15,20 +15,20 @@ try:
         decoded_data = data.decode('utf-8').strip()
         
         # Imprime y realiza funciones de acuerto al mando recibido        
-        if(decoded_data=='*31A'):
+        if(decoded_data=='*21A'):
             print("Ventanilla 1")
             # acá se debe agregar las otras rutinas para que haga el post
-            data = '{"ventanilla":"2","guiidOperador":"iU177A02EP-eb062dc8-0b44-4b21-8a57-b9a0df714af7-6L230u15LN-vn51AZ017K","codOficina":"11"}'
-            response = requests.post(url, headers=headers, data=data)
+            data = '{"guiidOperador":"fH09AI01ul-332469d2-0c30-4622-9f39-68c5442c0f7e-Gi23hz23RK-My54lO54Rz","ventanilla": "2","codOficina": "11"}'
+            response = requests.post(url, headers=headers, auth=('PwtZZTsCA8oMvuS54DgktZ6RbsOeggXJ', 'glzzsDFKwKk5WDy23RdIkaIkb31nS8kd'), data=data)
             print(response.status_code)
             print(response.text)
             # líneas abajo agregar rutina para el websocket y STOMP Protocol
             
-        elif (decoded_data=='*21A'):
+        elif (decoded_data=='*31A'):
             print("Ventanilla 2")
             # acá se debe agregar las otras rutinas para que haga el post
-            data = '{"ventanilla":"2","guiidOperador":"iU177A02EP-eb062dc8-0b44-4b21-8a57-b9a0df714af7-6L230u15LN-vn51AZ017K","codOficina":"11"}'
-            response = requests.post(url, headers=headers, data=data)
+            data = '{"guiidOperador":"iU177A02EP-eb062dc8-0b44-4b21-8a57-b9a0df714af7-6L230u15LN-vn51AZ017K","ventanilla": "2","codOficina": "11"}'
+            response = requests.post(url, headers=headers, auth=('PwtZZTsCA8oMvuS54DgktZ6RbsOeggXJ', 'glzzsDFKwKk5WDy23RdIkaIkb31nS8kd'), data=data)
             print(response.status_code)
             print(response.text)
             # líneas abajo agregar rutina para el websocket y STOMP Protocol
@@ -41,4 +41,4 @@ except KeyboardInterrupt:
 
 finally:
     # Cierra el puerto serial al finalizar
-    ser.close()
+    baseStation.close()
